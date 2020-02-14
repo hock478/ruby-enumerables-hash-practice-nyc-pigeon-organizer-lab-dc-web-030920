14db90b357a9c55b63cf01156b13c20b96516fa6
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-14 06:26:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,9 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
-  index = 0
-  
+  i = 0
+  while i < data[0].count do
+    pigeon_list[data[0][i]] = {}
+  end
+  pigeon_list
 end
