38e6a7d7fbc8fd1acdbd0ec6d1e3261cdff0069a
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-14 06:18:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,24 +2,5 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   index = 0
-  while index < data.size do
-    i = 0
-    while i < data[index].size do
-      data[index][i].each {|n| if pigeon_list[n]
-        pigeon_list[n][i].push(data[index][i])
-      else
-        pigeon_list[n] = {
-    :color => [],
-    :gender => [],
-    :lives => []
-  }
-  pigeon_list[n][i].push(data[index][i].stringify_keys)
-      end
-      }
-      i += 1
-    end
-    index += 1
-  end
   
-  pp pigeon_list
 end
