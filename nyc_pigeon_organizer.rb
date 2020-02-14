6b704fbb275c20bf35a8cad85f858b4c5bd6b19e
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-14 06:45:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,12 +9,13 @@ def nyc_pigeon_organizer(data)
    
    if !pigeon_list[name]
      pigeon_list[name] = {}
-  
+  end
      
    
    if !pigeon_list[name][key]
      pigeon_list[name][key] = []
-     
+   end  
+   
    pigeon_list[name][key] << val.to_s
    
    
